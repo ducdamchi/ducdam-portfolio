9576d20fafd4173b2e809802a2ccc45eafa49b18
Duc Dam <ducdamchi@github.com>
2025-01-31 17:28:36 -0500

    added initial html

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="navSection">
+        <span className="homeButton-ghost"></span>
+        <a href="index.html" target="_blank" rel="noopener noreferrer">Logo</a>
+        {/* <span class="home-button-real">Home</span> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <nav className="navBar">
+        <ul class="navBar-list">
+          <li>Photography</li>
+          <li>Film</li>
+          <li>Woodworking</li>
+          <li>About</li>
+        </ul>
+      </nav>
+
+      <h1 className="pageTitle">Photography</h1>
+
+      <div className="highlights">
+        <h2 className="sectionTitle">Highlights</h2>
+
+        {/* Making infinite loop carousel */}
+        <div className="carousel">
+          <button id="arrowLeft" className="arrow">
+            &#8249;
+          </button>
+
+          {/* TODO: How to automatically detect folders and render thumbnails? */}
+
+          <button id="arrowRight" className="arrow">
+            &#8250;
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+      <div className="allProjects">
+        <h2 className="sectionTitle">All Projects</h2>
+      </div>
+
+
     </>
   )
 }
-
-export default App
