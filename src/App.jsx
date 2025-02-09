a706b34e2bf53ca65ae7dd9bb0fd9602aba7e611
Duc Dam <ducdamchi@gmail.com>
2025-02-09 10:57:01 -0500

    carousel working now

--- src/App.jsx
+++ src/App.jsx
@@ -33,41 +33,79 @@ export default function App() {
 
     /* Highlight section will always have 9 thumbnails, split into 3 p-1 slides.
     Need a variable to keep track of which slide we're on so can navigate left/right.
-    1 is the max number of slides right now.
+    2 is the max number of slides right now.
     TODO: What about non-laptop devices? Responsive design solution?
     */
-    const maxIndex = 1;
 
-    /* To keep track of which slide we're on */
-    const [carouselIndex, setCarouselIndex] = useState(0);
-    console.log("Current carousel index: " + carouselIndex);
+    /* TO BE CHANGED FOR RESPONSIVE DESIGN */
+    /* From user's POV there are 3 slides with idx 0-2, 
+    but logically there are 5 slides with idx 0-4. */
+    const maxIndex = 4;
+    const ImagesPerPage = 3;
 
-    /* Use to store all the thumbnails, including clones */
-    // const [carousel, setCarousel] = useState([]);
 
-    const [clonesLeft, setClonesLeft] = useState([]);
-    const [clonesRight, setClonesRight] = useState([]);
+    /* STATES & VARS  */
+    const [carouselIndex, setCarouselIndex] = useState(1);  /* slide index we're on */
+    const [clonesLeft, setClonesLeft] = useState([]);    /* page shows up when slide left on first page. */
+    const [clonesRight, setClonesRight] = useState([]);   /* page shows up when slide right on last page. */
+    const [isEdgeTransition, setEdgeTransition] = useState(false)
 
-    /* For ease of modifying the thumbnails div*/
-    const thumbnails = useRef(null);
-
-    /* Making clones of first and last page of carousel to achieve infinite loop effect */
+    /* Update function for whenever carousel index changes */
     useEffect(() => {
+      console.log("Current index: " + carouselIndex);
+
+      /* If swipe right at last slide */
+      if (carouselIndex === maxIndex) {
+        setTimeout(() => {
+          setEdgeTransition(true);
+          setCarouselIndex(1);
+          // console.log("transitioned to real page 2");
+          }, 250)
+      }
+
+      /* If swipe left at first slide */
+      if (carouselIndex === 0) {
+        setTimeout(() => {
+          setEdgeTransition(true);
+          setCarouselIndex(3);
+          // console.log("transitioned to real page 2");
+          }, 250)
+      }
+
+      setTimeout(() => {
+        setEdgeTransition(false);
+      }, 300)
+
+    }, [carouselIndex])
+
+    /* Add ref tag to div that contains all thumbnails.
+    Make clones of first and last page of carousel */
+    const thumbnails = useRef(null);
+    useEffect(() => {
+
       /* Make sure thumbnails useRef object not null */
       if (thumbnails.current) {
 
         /* Select all thumbnail imgs, extract first three and last three */
         const allThumbnails = thumbnails.current.querySelectorAll('.thumbnail-img');
-        const firstThree = [...allThumbnails].slice(0, 3);
-        const lastThree = [...allThumbnails].slice(-3, 0);
+        const firstPage = [...allThumbnails].slice(0, ImagesPerPage);
+        const lastPage = [...allThumbnails].slice(-ImagesPerPage);
 
         /* Make clones of those two 'slides', will call clonesLeft/Right in HTML */
-        setClonesLeft(firstThree.map((thumbnail) => 
-          thumbnail.cloneNode(true)))
-        setClonesRight(lastThree.map((thumbnail) => 
-          thumbnail.cloneNode(true)))
+        setClonesLeft(lastPage.map((thumbnail) => thumbnail.src));
+        setClonesRight(firstPage.map((thumbnail) => thumbnail.src));
       }
     }, []);
+
+    /* Use 'style' attribute for thumbnails div for
+    ease of dynamically modifying 'carouselIndex' */
+    const thumbnails_style = {
+      '--slider-index': carouselIndex,
+      display: 'flex',
+      width: 'calc(100% - 2 * var(--slider-padding))',
+      transform: 'translateX(calc(var(--slider-index) * -100%))',
+      transition: isEdgeTransition? 'none' : 'transform 300ms ease-in-out'
+    }
 
     function nextSlide () {
       setCarouselIndex((prevIndex) => {
@@ -75,7 +113,6 @@ export default function App() {
         /* Loop back to first slide if at last slide */
         return newIndex > maxIndex ? 0 : newIndex;
       });
-
     }
 
     function prevSlide () {
@@ -106,9 +143,13 @@ export default function App() {
           {/* ALL THUMBNAILS
           TODO: How to automatically detect folders and render thumbnails?
           Hard coding images for now */}
-          <div ref = {thumbnails} className="flex w-[calc(100%-2*var(--slider-padding))]">
+          <div ref = {thumbnails} style={thumbnails_style}>
 
-            {}
+            {clonesLeft.map((src, index) => (
+              <div key={`cloneLeft-${index}`} className="flex-none w-1/3 p-1">
+                <img className="thumbnail-img-clone p-1 m-1" src={src}/>
+              </div>
+            ))}
 
             <div className="flex-none w-1/3 p-1">
               <img className="thumbnail-img p-1 m-1" src="./photography/ex1/img5.jpg"/>
@@ -137,6 +178,13 @@ export default function App() {
             <div className="flex-none w-1/3 p-1">
               <img className="thumbnail-img p-1 m-1" src="./photography/ex9/img1.jpg"/>
             </div>
+
+            {clonesRight.map((src, index) => (
+              <div key={`cloneRight-${index}`} className="flex-none w-1/3 p-1">
+                <img className="thumbnail-img-clone p-1 m-1" src={src}/>
+              </div>
+            ))}
+
           </div>
 
           {/* Right side button */}
