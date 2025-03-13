2f6ee1d9e1f432552e5c844b425076ee511106e5
Duc Dam <ducdamchi@gmail.com>
2025-03-13 11:51:55 -0400

    extend thumbnail box info to cloned slides

--- src/HighlightsThumbnails.jsx
+++ src/HighlightsThumbnails.jsx
@@ -6,7 +6,6 @@ import ModalViewer from './ModalViewer';
 
 export default function HighlightsThumbnails( {carouselIndex, isEdgeTransition, imageWidthPercent, imagesPerSlide}) {
 
-
   /*************** CSS **************/
   // aka the flex container for all the thumbnails
   const THUMBNAIL_FLEX_CONTAINER = {
@@ -47,52 +46,78 @@ export default function HighlightsThumbnails( {carouselIndex, isEdgeTransition, 
     /* Make sure thumbnails useRef object not null */
     if (thumbnails.current) {
 
-      //TODO: copy entire thumbnail box to clone, not just img
-
       /* Select all thumbnail imgs, extract first three and last three */
-      const allThumbnails = thumbnails.current.querySelectorAll('.thumbnail-img');
-      // console.log(allThumbnails);
-      const firstPage = [...allThumbnails].slice(0, imagesPerSlide);
-      const lastPage = [...allThumbnails].slice(-imagesPerSlide);
+      const images = thumbnails.current.querySelectorAll('.thumbnail-img');
+      const titles = thumbnails.current.querySelectorAll('.thumbnail-title');
+      const years = thumbnails.current.querySelectorAll('.thumbnail-year')
 
-      /* Make clones of those two 'slides', will call clonesLeft/Right in HTML */
-      setClonesLeft(lastPage.map((thumbnail) => thumbnail.src));
-      setClonesRight(firstPage.map((thumbnai) => thumbnai.src));
+      const firstImgs = [...images].slice(0, imagesPerSlide);
+      const lastImgs = [...images].slice(-imagesPerSlide);
+      const firstTitles = [...titles].slice(0, imagesPerSlide);
+      const lastTitles = [...titles].slice(-imagesPerSlide);
+      const firstYears = [...years].slice(0, imagesPerSlide);
+      const lastYears = [...years].slice(-imagesPerSlide);
+
+      /* Make clones of those two 'slides' to reference in HTML */
+      let clonesLeftLst = [];
+      for (let i=0; i < imagesPerSlide; i++) {
+        let clone_info = [];        
+        clone_info.push(lastImgs[i].src);
+        clone_info.push(lastTitles[i].innerHTML);
+        clone_info.push(lastYears[i].innerHTML);
+        clonesLeftLst.push(clone_info)
+      }
+
+      let clonesRightLst = [];
+      for (let i=0; i < imagesPerSlide; i++) {
+        let clone_info = [];        
+        clone_info.push(firstImgs[i].src);
+        clone_info.push(firstTitles[i].innerHTML);
+        clone_info.push(firstYears[i].innerHTML);
+        clonesRightLst.push(clone_info)
+      }
+    
+      // console.log("clones Left:", clonesLeftLst);
+      // console.log("clones Right:", clonesRightLst);
+
+      setClonesLeft(clonesLeftLst);
+      setClonesRight(clonesRightLst);
     }
   }, []);
 
   useEffect(() => {
-
-  }, [hoverId])
+    console.log(clonesLeft);
+  },[])
 
   return (
     <div ref={thumbnails} style={THUMBNAIL_FLEX_CONTAINER}>
       
       {/* Clones on left side */}
-      {clonesLeft.map((src, index) => (
+      {clonesLeft.map((cloneInfo, index) => (
         <div 
             key={`cloneLeft-${index}`} 
             className="thumbnail-flex-item" 
             style={THUMBNAIL_FLEX_ITEM}>
 
-              <div className="thumbnail-img-title-container relative border-3 border-green-500">
+            <div className="thumbnail-box border-4 border-orange-500">
+              <div className="thumbnail-info-container-clone relative border-3 border-green-500">
                 <img 
                   className="thumbnail-img-clone" 
-                  src={src}/>
+                  src={cloneInfo[0]}/>
 
                 <div
-                  className="thumbnail-title absolute bottom-15 left-6 text-2xl text-white font-extrabold">EXAMPLE PROJECT TITLE
+                  className="thumbnail-title absolute bottom-15 left-6 text-2xl text-white font-extrabold">{cloneInfo[1]}
                 </div>
 
                 <div
-                  className="thumbnail-year absolute bottom-8 left-6 text-lg text-white font-bold">2020-PRESENT
+                  className="thumbnail-year absolute bottom-8 left-6 text-lg text-white font-bold">{cloneInfo[2]}
                 </div> 
               </div>
-
+            </div>
         </div>
       ))}
       
-      {/* Iterate through each album and present thumbnails on slides */}
+      {/* Real slides: Iterate through each album and present its info */}
       {albumsData
         .filter((album) => album.isHighlight === true)
         .map((album) => (
@@ -106,7 +131,7 @@ export default function HighlightsThumbnails( {carouselIndex, isEdgeTransition, 
               onMouseEnter={() => setHoverId(album.id)}
               onMouseLeave={() => setHoverId(null)}>
 
-              <div className="thumbnail-img-title-container relative border-3 border-green-500">
+              <div className="thumbnail-info-container relative border-3 border-green-500">
                 <img 
                   className="thumbnail-img" 
                   src={album.thumbnail.src} 
@@ -144,26 +169,27 @@ export default function HighlightsThumbnails( {carouselIndex, isEdgeTransition, 
       ))}
 
       {/* Clones on right side */}
-      {clonesRight.map((src, index) => (
+      {clonesRight.map((cloneInfo, index) => (
         <div 
           key={`cloneRight-${index}`} 
           className="thumbnail-flex-item" 
           style={THUMBNAIL_FLEX_ITEM}>
 
-            <div className="thumbnail-img-title-container relative border-3 border-green-500">
-              <img 
-                className="thumbnail-img-clone" 
-                src={src}/>
+            <div className="thumbnail-box border-4 border-orange-500">
+              <div className="thumbnail-info-container-clone relative border-3 border-green-500">
+                <img 
+                  className="thumbnail-img-clone" 
+                  src={cloneInfo[0]}/>
 
-              <div
-                className="thumbnail-title absolute bottom-15 left-6 text-2xl text-white font-extrabold">EXAMPLE PROJECT TITLE
+                <div
+                  className="thumbnail-title absolute bottom-15 left-6 text-2xl text-white font-extrabold">{cloneInfo[1]}
+                </div>
+
+                <div
+                  className="thumbnail-year absolute bottom-8 left-6 text-lg text-white font-bold">{cloneInfo[2]}
+                </div> 
               </div>
-
-              <div
-                className="thumbnail-year absolute bottom-8 left-6 text-lg text-white font-bold">2020-PRESENT
-              </div> 
             </div>
-
         </div>
       ))}
 
