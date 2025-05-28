6727f40457da6625870578a7f54a00431ec2f519
Duc Dam <ducdamchi@gmail.com>
2025-05-27 23:10:01 -0400

    abc

--- src/Components/Photography/Photo_Landing.jsx
+++ src/Components/Photography/Photo_Landing.jsx
@@ -10,13 +10,21 @@ export default function Landing () {
   console.log(photoURL)
   
   /* Check if url is valid */
-  const all_urls = []
-  albumsData.forEach(album =>
-    all_urls.push(album.url)
-  )
-  const urls_str = all_urls.map(String)
+  // const all_urls = []
+  let curr_album 
+  let flag = 0
+  albumsData.forEach(album => {
+    console.log(album.id)
+    console.log(album.url)
+    if (album.url.toString() === photoURL) {
+      flag = 1
+      curr_album = album
+    }
+  }
+)
+  // const urls_str = all_urls.map(String)
 
-  if (!urls_str.includes(photoURL)) {
+  if (!flag) {
     return <div>Page not found</div>
   }
 
